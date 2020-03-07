67f5c738d61099fb0c0c1fc8d1e1b2ffd263c57b
Christopher Choquette Choo <choquette.christopher@gmail.com>
2020-03-06 21:03:14 -0500

    test as 2 separate jobs

--- a2/code/train_script.sh
+++ a2/code/train_script.sh
@@ -7,17 +7,22 @@
 #SBATCH --partition=gpu
 #SBATCH --output=slurm-train.out # specify output file
 #SBATCH --error=slurm-train.err  # specify error file
+#SBATCH --array=1-2
 
 eval "$(conda shell.bash hook)"
 conda activate 401a2
 
 TRAIN=/scratch/ssd001/home/cchoquet/csc401/a2/data/Hansard/Training/
 TEST=/scratch/ssd001/home/cchoquet/csc401/a2/data/Hansard/Testing/
-CELL_TYPE="gru"
+CELL_TYPE="lstm"
 #python a2_run.py vocab $TRAIN e vocab.e.gz
 #python a2_run.py vocab $TRAIN f vocab.f.gz
 python a2_run.py split $TRAIN train.txt.gz dev.txt.gz
-python a2_run.py train $TRAIN vocab.e.gz vocab.f.gz train.txt.gz dev.txt.gz model_wo_att.pt.gz --cell-type $CELL_TYPE --device cuda
-#python a2_run.py train $TRAIN vocab.e.gz vocab.f.gz train.txt.gz dev.txt.gz model_w_att.pt.gz --cell-type $CELL_TYPE --with-attention --device cuda
-#python a2_run.py train $TRAIN vocab.e.gz vocab.f.gz train.txt.gz dev.txt.gz model_w_att.pt.gz --cell-type $CELL_TYPE --with-attention --device cuda
-#python a2_run.py test $TEST vocab.e.gz vocab.f.gz model_w_att.pt.gz --cell-type $CELL_TYPE --with-attention --device cuda
+if [[ "${SLURM_ARRAY_TASK_ID}" == "1" ]]; then
+        python a2_run.py train $TRAIN vocab.e.gz vocab.f.gz train.txt.gz dev.txt.gz model_wo_att.pt.gz --cell-type $CELL_TYPE --device cuda
+        python a2_run.py test $TEST vocab.e.gz vocab.f.gz model_wo_att.pt.gz --device cuda
+elif [[ "${is_number}" == "2" ]]; then
+    python a2_run.py train $TRAIN vocab.e.gz vocab.f.gz train.txt.gz dev.txt.gz model_w_att.pt.gz --cell-type $CELL_TYPE --with-attention --device cuda
+    python a2_run.py test $TEST vocab.e.gz vocab.f.gz model_w_att.pt.gz --cell-type $CELL_TYPE --with-attention --device cuda
+
+
