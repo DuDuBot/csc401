5b28f80f532b4fdbd4e2e2810b8475dd175b0c74
Christopher Choquette Choo <choquette.christopher@gmail.com>
2020-03-06 21:26:21 -0500

    test as 2 separate jobs

--- a2/code/train_script.sh
+++ a2/code/train_script.sh
@@ -5,9 +5,9 @@
 #SBATCH --cpus-per-task=1                    # CPU cores/threads
 #SBATCH --qos=normal                        
 #SBATCH --partition=gpu
-#SBATCH --output=slurm-train.out # specify output file
-#SBATCH --error=slurm-train.err  # specify error file
-#SBATCH --array=1-2
+#SBATCH --output=train_%a.out # specify output file
+#SBATCH --error=train_%a.err  # specify error file
+#SBATCH --array=1,2
 
 eval "$(conda shell.bash hook)"
 conda activate 401a2
@@ -23,8 +23,8 @@ if [[ "${SLURM_ARRAY_TASK_ID}" == "1" ]]; then
     python a2_run.py train $TRAIN vocab.e.gz vocab.f.gz train.txt.gz dev.txt.gz model_wo_att.pt.gz --cell-type $CELL_TYPE --device cuda
     python a2_run.py test $TEST vocab.e.gz vocab.f.gz model_wo_att.pt.gz --device cuda
 elif [[ "${is_number}" == "2" ]]; then
-	python a2_run.py vocab $TRAIN e vocab2.e.gz
-	python a2_run.py vocab $TRAIN f vocab2.f.gz
+	# python a2_run.py vocab $TRAIN e vocab2.e.gz
+	# python a2_run.py vocab $TRAIN f vocab2.f.gz
     python a2_run.py split $TRAIN train2.txt.gz dev2.txt.gz
     python a2_run.py train $TRAIN vocab2.e.gz vocab2.f.gz train2.txt.gz dev2.txt.gz model_w_att.pt.gz --cell-type $CELL_TYPE --with-attention --device cuda
     python a2_run.py test $TEST vocab2.e.gz vocab2.f.gz model_w_att.pt.gz --cell-type $CELL_TYPE --with-attention --device cuda
